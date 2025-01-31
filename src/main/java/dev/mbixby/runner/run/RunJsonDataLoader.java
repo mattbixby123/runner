c78c7a4e1b9485d476da1e55927b1de000ac8e9a
mattbixby123 <matthew.bixby1@gmail.com>
2025-01-31 15:39:24 -0500

    started off by replacing in memory collection with an H2 embedded database. We set up the jdbc client oto get data in and out of the H2 database.  And then we moved on and we replaced our H2 database with a PostgreSQL db - finished up by looking at spring data

---
+++ src/main/java/dev/mbixby/runner/run/RunJsonDataLoader.java
@@ -0,0 +1,42 @@
+package dev.mbixby.runner.run;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class RunJsonDataLoader implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(RunJsonDataLoader.class);
+    private final JdbcClientRunRepository runRepository;
+    private final ObjectMapper objectMapper;
+
+    // Dependency injection : creating an instance of the class of run repository
+    public RunJsonDataLoader(JdbcClientRunRepository runRepository, ObjectMapper objectMapper) {
+        this.runRepository = runRepository;
+        // and object mapper (for reading mapping and deserializing json into list of runs)
+        this.objectMapper = objectMapper;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (runRepository.count() == 0) {
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream("/data/runs.json")) {
+                Runs allRuns = objectMapper.readValue(inputStream, Runs.class);
+                log.info("Reading {} runs from JSON data and saving to a database.", allRuns.runs().size());
+                runRepository.saveAll(allRuns.runs());
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to read JSON data", e);
+            }
+        } else {
+            log.info("Not loading Runs from JSON data because the collection contains data.");
+        }
+    }
+}
